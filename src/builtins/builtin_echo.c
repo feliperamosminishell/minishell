4d9f0d67b740d921b93d671defdfae92e3bcaf13
Gonzalo Ramos Lopez <gonzaloramoslopez@MacBook-Air-de-Gonzalo.local>
2026-02-03 03:53:09 +0100

    correccion echo con casos edge

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -6,63 +6,49 @@
 /*   By: goramos- <goramos-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 00:37:01 by goramos-          #+#    #+#             */
-/*   Updated: 2026/02/03 03:42:55 by goramos-         ###   ########.fr       */
+/*   Updated: 2026/02/03 03:52:21 by goramos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../minishell.h"
 
-static int	echo_aux (char *arg)
+static int	is_valid_n_flag(char *arg)
 {
 	int	i;
 
 	i = 0;
 	if (arg[i] != '-')
-		return (10);
-	else
-	{
-		i++;
-		while(arg)
-		{
-			if (arg[i] == 'n')
-				i++;
-			if (arg[i] != 'n' && arg[i] != '\0' && !(arg[i - 1] == '-'))
-				return (10);
-			if ((arg[i - 1] == 'n') && arg[i] == '\0')
-				return (20);
-		}
 		return (0);
-	}
+	i++;
+	if (arg[i] != 'n')
+		return (0);
+	while (arg[i] == 'n')
+		i++;
+	if (arg[i] == '\0')
+		return (1);
+	return (0);
 }
 
 int	builtin_echo(char **argv)
 {
 	int	i;
 	int	flag_nl;
-	int	check;
-	int	j;
 
-	flag_nl = 0;
-	i = 1;//PREGUNTAR FELIPE POR TEMA PARSEO COMO ME LLEGA SI CMD + ARGS o solo ARGS
-	while(argv[i])
+	flag_nl = 1;
+	i = 1;
+	while (argv[i] && is_valid_n_flag(argv[i]))
 	{
-		j = echo_aux(argv[i]);
-		if (j == 10)
-			break;
-		else if (j == 20)
-		{
-			flag_nl = 1;
-		}
+		flag_nl = 0;
 		i++;
 	}
 	while (argv[i])
 	{
-		printf("%s",argv[i]);
+		printf("%s", argv[i]);
 		if (argv[i + 1])
 			printf(" ");
 		i++;
 	}
-	if (flag_nl == 0)
+	if (flag_nl)
 		printf("\n");
 	return (0);
 }
